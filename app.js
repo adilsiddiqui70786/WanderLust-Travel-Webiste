da90a463bd559bd69106743bee7a09dcb5c648f0
adilsiddiqui70786 <siddiquiadil70786@gmail.com>
2024-02-06 18:11:47 +0530

    delete some unusual

--- app.js
+++ app.js
@@ -51,13 +51,10 @@ app.use(express.static(path.join(__dirname, "/public")));
 
 const store = MongoStore.create({
 	mongoUrl: dbURL,
-	crypto: {
-		secret: process.env.SECRET,
-	},
 	touchAfter: 24 * 3600,
 });
 
-store.on("error", () => {
+store.on("error", (err) => {
 	console.log("ERROR in MONGO SESSION STORE", err);
 });
 
